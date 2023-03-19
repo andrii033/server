91a8d46653ef57f2ec2eb2915e5195ea7f0bd670
andrii033 <ataran033@gmail.com>
2023-03-19 10:04:40 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jaelf/Documents/cpp_prog/server
-# Build directory: /home/jaelf/Documents/cpp_prog/server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
