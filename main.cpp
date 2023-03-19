ea0bfb45e3e24fc0299769c0e097ef9721c0d464
andrii <ataran033@gmail.com>
2023-03-19 10:27:05 +0100

    create table

--- main.cpp
+++ main.cpp
@@ -7,12 +7,19 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
 
 using namespace std;
 
 #define PORT 8080
 
 void *connection_handler(void *);
+void create_table();
 
 int main() {
     int server_fd, new_socket;
@@ -90,4 +97,48 @@ void *connection_handler(void *socket_desc) {
     close(sock);
 
     return 0;
+}
+
+void create_table()
+{
+    try
+    {
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+        sql::ResultSet *res;
+
+        /* Create a connection */
+        driver = get_driver_instance();
+        con = driver->connect("tcp://localhost:3306", "debian-sys-maint", "PUAFKnCbBczs8rLS");
+        // con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+        /* Connect to the MySQL test database */
+        con->setSchema("my");
+
+        stmt = con->createStatement();
+        res = stmt->executeQuery("SELECT 'Hello World!' AS _message"); // replace with your statement
+        while (res->next())
+        {
+            cout << "\t... MySQL replies: ";
+            /* Access column data by alias or column name */
+            cout << res->getString("_message") << endl;
+            cout << "\t... MySQL says it again: ";
+            /* Access column fata by numeric offset, 1 is the first column */
+            cout << res->getString(1) << endl;
+        }
+        delete res;
+        delete stmt;
+        delete con;
+    }
+    catch (sql::SQLException &e)
+    {
+        cout << "# ERR: SQLException in " << __FILE__;
+        // cout << "(" << __FUNCTION__ << ") on line "
+        //      << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+    }
+
+    cout << endl;
 }
